dcf3714d94cef272a54e24b316cf3ea58ec48046
AmarRed07 <redzepagicamar@yahoo.com>
2020-12-04 15:22:11 -0800

    final

--- loops.js
+++ loops.js
@@ -1,10 +1,8 @@
-// Create a new file called loops.js and link it to your HTML file. Create the following loops with 
-// any variables you feel are needed:
-console.log ('while loops ');
-console.log ('a');
+console.log('while loops ');
+console.log('a');
 var i = 0; 
 
-while (i < 100) { 
+while(i < 100) { 
    i += 2; 
    console.log(i); 
 } 
@@ -12,31 +10,37 @@ while (i < 100) {
 console.log('b');
 var i = 100
 
-// how to get it to stop at 0
-// figured it out but why is i++ outside of brackets
-while (i > 0) {
+while(i > 0) {
     (i -= 3);
     console.log(i);
-    if (i <= 1) {
+    if(i <= 1) {
         break;
     }
-    i++; // why outside brackets?
+    i++;
 }
 
 
-console.log ('For loops ');
-console.log ('c ');
+console.log('For loops ');
+console.log('c ');
 
-for (let i =1; i < 100; i++) {
-    if (i += 2); {
+for(let i =1; i < 100; i++) {
+    if(i += 2); {
         console.log(i);
     }
 }
 
-console.log ('d');
+console.log('d');
 
-for (let i = 0; i < 100; i++) {
-    if(!i%3) {
+
+for (let i =0; i < 100; i++)
+    if(i%3 ==0) {
+        console.log('Hello');
+    } 
+    else if(i%5 == 0) {
+        console.log('World');
+    }
+    else if (i%3 !== 0 && i%5 !== 0) {
+        console.log('Hello World');
+    } else {
         console.log(i);
     }
-}
